4bdff8f709ddb3ec7819efd97a4badb45fa4fd65
maple8192 <57043077+maple8192@users.noreply.github.com>
2024-07-07 19:05:15 +0900

    テンプレート盆栽

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,198 @@
-#![allow(dead_code)]
-#![allow(unused_imports)]
+#![allow(dead_code, unused_imports, unused_macros)]
+use std::borrow::BorrowMut;
 
-use std::cmp::{max, min};
-use std::collections::{BinaryHeap, BTreeMap, BTreeSet, VecDeque};
-use std::iter::once;
-use ac_library::{Additive, Dsu, DynamicModInt, Max, Min, ModInt1000000007, ModInt998244353, Monoid, Multiplicative, Segtree};
-use bstr::ByteSlice;
-use easy_ext::ext;
-use itertools::Itertools;
-use itertools_num::ItertoolsNum;
-use num_integer::{gcd, gcd_lcm};
-use num_rational::Ratio;
-use num_traits::{FromPrimitive, ToPrimitive};
-use omniswap::swap;
-use proconio::{fastout, input};
-use proconio::marker::{Bytes, Usize1};
-use rustc_hash::{FxHashMap, FxHashSet};
-use superslice::Ext;
+use my_library::*;
 
+#[fastout]
 fn main() {
     input! {
 
     }
 }
 
-const INF: usize = 1_000_000_000_000_000_000;
+mod my_library {
+    pub use std::iter::once;
+    pub use std::collections::{BinaryHeap, BTreeMap, BTreeSet, VecDeque};
+    pub use ac_library::*;
+    pub use bstr::ByteSlice;
+    pub use itertools::Itertools;
+    pub use num_rational::Ratio;
+    pub use num_traits::{FromPrimitive, ToPrimitive};
+    pub use omniswap::swap;
+    pub use proconio::{input, fastout, marker::{Bytes, Usize1}};
+    pub use rustc_hash::{FxHashMap as HashMap, FxHashSet as HashSet};
+    pub use superslice::Ext;
 
-const DIR4: [(isize, isize); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
-const DIR8: [(isize, isize); 8] = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)];
+    use std::ops::{AddAssign, MulAssign};
+    use num::{One, Zero};
+    use easy_ext::ext;
+    use proconio::source::Readable;
 
-#[ext]
-impl usize {
-    fn is_prime(&self) -> bool {
-        if *self == 0 || *self == 1 { return false }
-        (2..).take_while(|&x| x * x <= *self).all(|x| *self % x != 0)
-    }
+    pub const INF: usize = 1_000_000_000_000_000_000;
 
-    fn divisors(&self) -> Vec<usize> {
-        let mut div = vec![];
-        for i in (1..).take_while(|&x| x * x <= *self) {
-            if *self % i != 0 { continue }
-            div.push(i);
-            if *self / i != i { div.push(*self / i) }
+    pub const DIR4: [(isize, isize); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+    pub const DIR8: [(isize, isize); 8] = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)];
+
+    #[ext]
+    impl usize {
+        fn is_prime(&self) -> bool {
+            if *self == 0 || *self == 1 { return false }
+            (2..).take_while(|&x| x * x <= *self).all(|x| *self % x != 0)
         }
-        div
-    }
 
-    fn factors(&self) -> Vec<(usize, usize)> {
-        let mut fact = vec![];
-        let mut n = *self;
-        for i in 2..=*self {
-            if i * i > n { break }
-            if n % i != 0 { continue }
-            let mut e = 0;
-            while n % i == 0 {
-                e += 1;
-                n /= i;
+        fn divisors(&self) -> Vec<usize> {
+            let mut ret = vec![];
+            for i in (1..).take_while(|&i| i * i <= *self).filter(|i| *self % i == 0) {
+                ret.push(i);
+                if *self / i != i { ret.push(*self / i) }
             }
-            fact.push((i, e));
+            ret
         }
-        if n != 1 { fact.push((n, 1)) }
-        fact
-    }
-}
 
-#[ext]
-impl bool {
-    fn yes_no(&self) -> &str {
-        if *self { "Yes" } else { "No" }
-    }
-}
+        fn factors(&self) -> Vec<(usize, usize)> {
+            let mut ret = vec![];
+            let mut n = *self;
+            for i in 2..*self {
+                if i * i > n { break }
+                if n % i != 0 { continue }
 
-fn eratosthenes(n: usize) -> Vec<bool> {
-    let mut vec = vec![true; n + 1];
-    vec[0] = false;
-    vec[1] = false;
-    for i in (2..).take_while(|&x| x * x <= n) {
-        if !vec[i] { continue }
-        for j in (2..).take_while(|&j| i * j <= n) {
-            vec[i * j] = false;
+                let mut e = 0;
+                while n % i == 0 {
+                    n /= i;
+                    e += 1;
+                }
+                ret.push((i, e));
+            }
+            if n != 1 { ret.push((n, 1)) }
+            ret
         }
     }
-    vec
-}
 
-fn compress<T: Ord>(vec: &[T]) -> Vec<usize> {
-    let set = vec.iter().collect::<BTreeSet<_>>();
-    let map = set.into_iter().enumerate().map(|(i, n)| (n, i)).collect::<BTreeMap<_, _>>();
-    let mut ret = vec![];
-    for n in vec {
-        ret.push(map[n]);
+    #[ext]
+    impl bool {
+        fn yes_no(&self) -> &str {
+            if *self { "Yes" } else { "No" }
+        }
     }
-    ret
-}
 
-fn power_by<T, F: Fn(&T, &T) -> T>(mut a: T, mut n: usize, e: T, f: F) -> T {
-    let mut ans = e;
-    while n > 0 {
-        if n % 2 != 0 { ans = f(&ans, &a) }
-        a = f(&a, &a);
-        n /= 2;
+    pub fn cumsum<T: Zero + AddAssign + Copy>(vec: &Vec<T>) -> Vec<T> {
+        once(&T::zero()).chain(vec).scan(T::zero(), |acc, &x| { *acc += x; Some(*acc) }).collect_vec()
     }
-    ans
+
+    pub fn cumprod<T: One + MulAssign + Copy>(vec: &Vec<T>) -> Vec<T> {
+        once(&T::one()).chain(vec).scan(T::one(), |acc, &x| { *acc *= x; Some(*acc) }).collect_vec()
+    }
+
+    pub fn eratosthenes(n: usize) -> Vec<bool> {
+        if n == 0 { return vec![false] }
+        let mut ret = vec![true; n + 1];
+        ret[0] = false;
+        ret[1] = false;
+        for i in (2..).take_while(|i| i * i <= n) {
+            if !ret[i] { continue }
+            for j in (2..).take_while(|j| i * j <= n) {
+                ret[i * j] = false;
+            }
+        }
+        ret
+    }
+
+    pub fn compress<T: Ord>(vec: &[T]) -> Vec<usize> {
+        let set = vec.iter().collect::<BTreeSet<_>>();
+        let map = set.into_iter().enumerate().map(|(i, n)| (n, i)).collect::<BTreeMap<_, _>>();
+        let mut ret = vec![];
+        for n in vec {
+            ret.push(map[n]);
+        }
+        ret
+    }
+
+    pub fn power_by<T, F: Fn(&T, &T) -> T>(mut a: T, mut n: usize, e: T, f: F) -> T {
+        let mut ans = e;
+        while n > 0 {
+            if n % 2 != 0 { ans = f(&ans, &a) }
+            a = f(&a, &a);
+            n /= 2;
+        }
+        ans
+    }
+
+    #[macro_export]
+    macro_rules! min {
+        ($a:expr $(,)*) => {{
+            $a
+        }};
+        ($a:expr, $b:expr $(,)*) => {{
+            std::cmp::min($a, $b)
+        }};
+        ($a:expr, $($rest:expr),+ $(,)*) => {{
+            std::cmp::min($a, min!($($rest),+))
+        }};
+    }
+
+    #[macro_export]
+    macro_rules! max {
+        ($a:expr $(,)*) => {{
+            $a
+        }};
+        ($a:expr, $b:expr $(,)*) => {{
+            std::cmp::max($a, $b)
+        }};
+        ($a:expr, $($rest:expr),+ $(,)*) => {{
+            std::cmp::max($a, max!($($rest),+))
+        }}
+    }
+
+    #[macro_export]
+    macro_rules! gcd {
+        ($a:expr $(,)*) => {{
+            $a
+        }};
+        ($a:expr, $b:expr $(,)*) => {{
+            num_integer::gcd($a, $b)
+        }};
+        ($a:expr, $($rest:expr),+ $(,)*) => {{
+            num_integer::gcd($a, gcd!($($rest),+))
+        }};
+    }
+
+    #[macro_export]
+    macro_rules! lcm {
+        ($a:expr $(,)*) => {{
+            $a
+        }};
+        ($a:expr, $b:expr $(,)*) => {{
+            num_integer::lcm($a, $b)
+        }};
+        ($a:expr, $($rest:expr),+ $(,)*) => {{
+            num_integer::lcm($a, lcm!($($rest),+))
+        }};
+    }
+
+    fn find_furthest_vertex(tree: &[Vec<(usize, usize)>], start: usize) -> (usize, usize) {
+        let mut dist = HashMap::from_iter([(start, 0)]);
+        let mut queue = VecDeque::from_iter([start]);
+        while let Some(v) = queue.pop_front() {
+            for &(nv, cost) in &tree[v] {
+                if dist.contains_key(&nv) { continue }
+                dist.insert(nv, dist[&v] + cost);
+                queue.push_back(nv);
+            }
+        }
+        dist.into_iter().max_by_key(|&(_, d)| d).unwrap()
+    }
+
+    pub fn tree_diameter(tree: &[Vec<(usize, usize)>]) -> (usize, usize) {
+        let (start, _) = find_furthest_vertex(tree, 0);
+        find_furthest_vertex(tree, start)
+    }
+
+    pub enum Offset<const N: usize> {}
+    impl<const N: usize> Readable for Offset<N> {
+        type Output = usize;
+        fn read<R: std::io::BufRead, S: proconio::source::Source<R>>(source: &mut S) -> Self::Output {
+            (isize::read(source) + N as isize) as usize
+        }
+    }
+    pub type Offset10e9 = Offset<1_000_000_000>;
 }
